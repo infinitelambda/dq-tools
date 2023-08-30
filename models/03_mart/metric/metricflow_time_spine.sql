393094892fb266670c936a12513d228b5b3ecaf1
Dat Nguyen <dat@infinitelambda>
2023-08-30 16:30:57 +0700

    fix: optimize metricflow_time_spine

--- models/03_mart/metric/metricflow_time_spine.sql
+++ models/03_mart/metric/metricflow_time_spine.sql
@@ -6,15 +6,19 @@
 }}
 
 --Check https://docs.getdbt.com/docs/build/metricflow-time-spine
+{% set start_date = var("dbt_dq_tool_start_date", "to_date('01/01/2000','mm/dd/yyyy')") -%}
+{% set end_date = var("dbt_dq_tool_end_date", "to_date('01/01/2030','mm/dd/yyyy')") -%}
+
+{%- if target.type == "bigquery" %}
+
+  {% set start_date = var("dbt_dq_tool_start_date", "DATE(2000,01,01)") %}
+  {% set end_date = var("dbt_dq_tool_end_date", "DATE(2030,01,01)") %}
+
+{%- endif %}
+
 with days as (
 
-    {{
-        dbt_utils.date_spine(
-            'day',
-            "to_date('01/01/2000','mm/dd/yyyy')",
-            "to_date('01/01/2027','mm/dd/yyyy')"
-        )
-    }}
+  {{ dbt_utils.date_spine('day', start_date, end_date) }}
 
 ),
 
