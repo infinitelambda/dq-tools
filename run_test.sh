7e7674bf485cfcdcb86b83d1a33eb7d01223c758
Dat Nguyen <dat@infinitelambda>
2023-08-30 16:06:59 +0700

    feat: adjust run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -46,6 +46,6 @@ echo -e "${BLUE}3: Verify log table / Turn warns as errors${NC}"
 dbt --warn-error test --select source:dq_tools_test --target $1 || exit 1
 
 echo -e "${BLUE}4: Make sure the metrics working {NC}"
-dbt parse
+dbt parse --target $1 || exit 1
 mf list metrics
-mf query --metrics data_quality_score --group-by key__run_time key__dq_dimension
+mf query --metrics data_quality_score --group-by key__run_time --group-by key__dq_dimension --explain
