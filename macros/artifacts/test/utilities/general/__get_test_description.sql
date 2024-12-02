d8e94d86ee70d330d38d22e44acae6f3cd307d1a
Dat <dat@infinitelambda.com>
2024-12-02 13:54:16 +0700

    feat: adj auto_generate_test_description

--- macros/artifacts/test/utilities/general/__get_test_description.sql
+++ macros/artifacts/test/utilities/general/__get_test_description.sql
@@ -6,27 +6,27 @@
 
 {% macro default____get_test_description(test_node) %}
 
-    {%- set test_type = test_node.test_metadata.name -%}
-    {%- set column_name = test_node.test_metadata.kwargs.column_name -%}
-    {%- set testing_model = dq_tools.__get_test_model(test_node) -%}
-    {%- set model_name = dq_tools.__get_relation(testing_model).name | lower -%}
-
     {% if test_node.description is defined and test_node.description %}
 
         {{ return(test_node.description) }}
     
-    {% else %}
+    {% elif dq_tools.__get_test_type(test_node) != 'singular' and var("dq_tools__auto_generate_test_description", "0") == "1" %}
+
+        {%- set test_name = dq_tools.__get_test_name(test_node) -%}
+        {%- set column_name = dq_tools.__get_column_name(test_node) -%}
+        {%- set testing_model = dq_tools.__get_test_model(test_node) -%}
+        {%- set model_name = dq_tools.__get_relation(testing_model).name | lower -%}
 
         {%- set generated_description = '' -%}
 
-        {%- if test_type == 'unique' -%}
+        {%- if test_name == 'unique' -%}
             {%- set generated_description = 'The ' ~ column_name ~ ' column in the ' ~ model_name ~ ' model should be unique.' -%}
-        {%- elif test_type == 'not_null' -%}
+        {%- elif test_name == 'not_null' -%}
             {%- set generated_description = 'The ' ~ column_name ~ ' column in the ' ~ model_name ~ ' model should not contain null values.' -%}
-        {%- elif test_type == 'accepted_values' -%}
+        {%- elif test_name == 'accepted_values' -%}
             {%- set accepted_values = test_node.test_metadata.kwargs['values'] | join(', ') -%}
             {%- set generated_description = 'The ' ~ column_name ~ ' column in the ' ~ model_name ~ ' should be one of ' ~ accepted_values ~ ' values.' -%}
-        {%- elif test_type == 'relationships' -%}
+        {%- elif test_name == 'relationships' -%}
             {%- set to_model = test_node.test_metadata.kwargs.to -%}
             {%- set related_model = to_model.split('\'')[1].strip() -%}
             {%- set generated_description = 'Each ' ~ column_name ~ ' in the ' ~ model_name ~ ' model exists as an id in the ' ~ related_model ~ ' table.' -%}
