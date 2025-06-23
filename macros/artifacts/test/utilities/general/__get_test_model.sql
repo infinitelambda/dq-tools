4af930671af5c768502f1f28c04ad19fb8c80245
Pradit Rattanapongchinda <pradit.ra@tdshop.io>
2025-06-23 12:31:43 +0700

    fix: get relation correctly when model has a version

--- macros/artifacts/test/utilities/general/__get_test_model.sql
+++ macros/artifacts/test/utilities/general/__get_test_model.sql
@@ -15,27 +15,47 @@
 
     {#
       relation_func = "source('name', 'table')" or
-      relation_func = "ref('table')" or "ref('package', 'table')"
+      relation_func = "ref('table')" or "ref('package', 'table')" or "ref('package', 'table', v='1')" or "ref('table', v='1')"
     #}
     {% set relation_func = result.group(1) ~ ')' %}
-
     {% set parts = relation_func.split(',') %}
     {% if relation_func.startswith('ref') %}
-
       {% if parts | length > 1 %}
-        {{ return(
-          {
-            'type': 'ref',
-            'name': re.search('\'(.*?)\'', parts[1]).group(1),
-            'package_name': re.search('\'(.*?)\'', parts[0]).group(1)
-          }
-        )}}
+        {% if 'version=' in relation_func %}
+          {% if parts | length == 2 %}
+            {{ return(
+            {
+              'type': 'ref',
+              'package_name': '',
+              'name': re.search('\'(.*?)\'', parts[0]).group(1),
+              'version': re.search('version=\'(.*?)\'', parts[1]).group(1)
+            }
+          )}}
+          {% elif parts | length == 3 %}
+          {{ return(
+            {
+              'type': 'ref',
+              'package_name': re.search('\'(.*?)\'', parts[0]).group(1),
+              'name': re.search('\'(.*?)\'', parts[1]).group(1),
+              'version': re.search('version=\'(.*?)\'', parts[2]).group(1)
+            }
+          )}}
+          {% endif %}  
+        {% else %}
+          {{ return(
+            {
+              'type': 'ref',
+              'package_name': re.search('\'(.*?)\'', parts[0]).group(1),
+              'name': re.search('\'(.*?)\'', parts[1]).group(1)
+            }
+          )}}
+        {% endif %}
       {% else %}
         {{ return(
           {
             'type': 'ref',
-            'name': re.search('\'(.*?)\'', parts[0]).group(1),
-            'package_name': ''
+            'package_name': '',
+            'name': re.search('\'(.*?)\'', parts[0]).group(1)
           }
         )}}
       {% endif %}
@@ -49,6 +69,7 @@
           'source_name': re.search('\'(.*?)\'', parts[0]).group(1)
         }
       )}}
+
 
     {% endif %}
 
